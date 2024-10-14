201721a139d893539a7f03e82544f621b20f7206
gnawbin <1046450819@qq.com>
2024-10-14 16:00:26 +0800

    第一版

---
+++ numerical_algorithm/total_pivoting_gaussian_elimination.h
@@ -0,0 +1,12 @@
+//
+// Created by one on 2024/10/14.
+//
+
+#ifndef TOTAL_PIVOTING_GAUSSIAN_ELIMINATION_H
+#define TOTAL_PIVOTING_GAUSSIAN_ELIMINATION_H
+/**
+* 全选主元高斯消去法
+*/
+#include "Complex.h"
+
+#endif //TOTAL_PIVOTING_GAUSSIAN_ELIMINATION_H
